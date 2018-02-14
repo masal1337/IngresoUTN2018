bbc8354d4f38adf9365cef1600794e63c16b3e55
masal1337 <36115778+masal1337@users.noreply.github.com>
2018-02-14 10:32:40 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,22 @@
-function Mostrar()
-{
+function Mostrar() {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+	var numeroRandom = Math.floor(Math.random() * 10) + 1;
+
+	if (numeroRandom > 8) {
+
+		alert(numeroRandom + " ¡Excelente!");
+	}
+
+	else if (numeroRandom >= 4) {
+
+		alert(numeroRandom + " ¡Aprobò!");
+	}
+
+	else {
+
+		alert(numeroRandom + " ¡Vamos, la proxima se puede!");
+
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,5 +1,6 @@
-function Mostrar()
-{
+function Mostrar() {
 	//Genero el número RANDOM entre 1 y 10 
-	var numeroRandom = document.getElementById()
+	var numeroRandom = Math.floor(Math.random() * 10) + 1;
+
+alert(numeroRandom);
 }//FIN DE LA FUNCIÓN
