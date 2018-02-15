f4f2e8254213e19854d9953c58f180d70ebe03a5
masal1337 <36115778+masal1337@users.noreply.github.com>
2018-02-14 22:47:27 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,33 @@
-function Mostrar()
-{
-//tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+function Mostrar() {
+    //tomo el mes 
+    var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+
+    switch (mesDelAño) {
+        case "Febrero":
+
+
+            alert(" ¡" + mesDelAño + " tiene 28 días!");
+            break;
+
+
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+
+
+            alert(" ¡" + mesDelAño + " tiene 30 días!");
+            break;
+
+        default:
+
+
+            alert(" ¡" + mesDelAño + " tiene 31 días!");
+
+    }
+
+
 
 
 
