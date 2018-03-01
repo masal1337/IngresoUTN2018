325a73ccab14d5ce90dbfbb847b81938fce0eca1
masal1337 <36115778+masal1337@users.noreply.github.com>
2018-03-01 12:21:58 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/6-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/6-iteraciones.js
@@ -1,47 +1,46 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
-function Mostrar() {   
+function Mostrar() {
 
 
-{
-   
-    var importe = parseInt(prompt("Ingrese un importe mayor a 0"));
-    var impMenor = 100;
-    var impMayor = 0;
-    var i = 1;
-    var flag = 0;
+    {
 
-  
-  for (i ; 1 <= 7; i++)
-  
-  importe = parseFloat(prompt("Ingrese un importe mayor a 0"));
+        var importe;
+        var impMenor;
+        var impMayor;
+        var flag = 0;
 
-  while (importe <= 0) {
 
-importe = parseFloat(prompt("Error. Usted tiene que ingresar un numero mayor a 0"));
+        for (var i = 1; i <= 7; i++) {
+
+            importe = parseFloat(prompt("Ingrese un importe mayor a 0"));
+
+            while (importe <= 0) {
+
+                importe = parseFloat(prompt("Error. Usted tiene que ingresar un numero mayor a 0"));
+
+            }
+
+            if (importe > impMayor || flag == 0) {
+
+                impMayor = importe;
+
+
+            }
+
+            if (importe < impMenor || flag == 0) {
+
+                impMenor = importe;
+                flag = 1;
+
+            }
 
         }
-
-        if (importe > impMayor) {
-
-            impMayor = importe;
-            flag = 1;
-impMayor = importe;
-        }
-
-        else if (importe < impMayor || flag == 0) {
-
-            impMenor = importe;
-            flag = 1;
-
-        }
-
-        
 
     }
 
 
 
-alert("Su importe mayor es de " + impMayor + " y su importe menor es de " + impMenor);
+    alert("Su importe mayor es de " + impMayor + " y su importe menor es de " + impMenor);
 
 
 
--- ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
@@ -1,6 +1,53 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
-function Mostrar()
-{
-	
+function Mostrar() {
+    var nota;
+    var sexo;
+    var acumNotas = 0;
+    var promedio;
+    var notaBaja;
+    var varonesMas6 = 0;
+    var flag = 0;
+   
+    
+    
+    for (var i = 1; i < 7; i++) {
+
+        nota = parseInt(prompt("Ingrese la nota del alumno."));
+
+        while (nota < 0 || nota > 10) {
+
+
+            nota = parseInt(prompt("Error. La nota es mayor que 10 o menor que 0. Reingrese: "));
+
+        }
+        acumNotas +nota;
+
+        sexo = prompt("Ingrese el sexo. ");
+        sexo = sexo.toLowerCase();
+        
+
+        while (sexo != "m" && sexo != "f")
+
+
+            sexo = prompt("Error. Reingrese. Ese sexo no existe");
+
+
+    }
+    if (sexo == "f") {
+        sexo = "masculino";
+
+    }
+    else {
+
+        sexo = "femenino";
+
+    }
+
+promedio = acumNotas / 6;
+
+alert (promedio);
+
+
 }
+
 
